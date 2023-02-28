0b7875aecfd19f4773e69b2b129d2473bc173867
cbarriuso <carlosbarriuso@yahoo.es>
2023-02-28 19:51:16 +0100

    variable de entorno

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Heroe } from '../interfaces/heroes.interface';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import { environments } from 'src/environments/environments';
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +11,19 @@ import { ActivatedRoute } from '@angular/router';
 export class HeroesService {
 
   //heroes: Heroe[] = [];
-
+  private baseUrl: string = environments.baseUrl;
   constructor(
     private http: HttpClient,
     ) { }
 
   getHeroes(): Observable<Heroe[]>{
     //Mandamos un Observable a donde sea mandado
-    return this.http.get<Heroe[]>('http://localhost:3000/heroes');
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
   getHeroePorId( id:string ): Observable<Heroe>{
 
-    return this.http.get<Heroe>(`http://localhost:3000/heroes/${id}`);
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
 
   }
 }
