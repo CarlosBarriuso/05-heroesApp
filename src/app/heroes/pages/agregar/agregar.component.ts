5002a5269ec2e3890df6d59ffffe46364812fa0e
cbarriuso <carlosbarriuso@yahoo.es>
2023-03-03 17:12:02 +0100

    editar heroe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from "rxjs/operators";
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
+import { HeroesService } from '../../services/heroes.service';
 
 
 
@@ -8,7 +11,7 @@ import { Heroe, Publisher } from '../../interfaces/heroes.interface';
   templateUrl: './agregar.component.html',
 
 })
-export class AgregarComponent {
+export class AgregarComponent implements OnInit{
 
     plublishers = [
       {
@@ -28,5 +31,49 @@ export class AgregarComponent {
       first_appearance: '',
       publisher: Publisher.DCComics,
       alt_img: ''
+    }
+
+  constructor( private heroesService: HeroesService,
+                  private activatedRoute: ActivatedRoute,
+                  private router: Router) {}
+
+  ngOnInit(): void {
+
+    //verificar el id
+    this.activatedRoute.params
+    .pipe(
+      //para sacar la info del heroe, primero obtenemos el heroe por el id
+      switchMap ( ({id}) => this.heroesService.getHeroePorId(id) )
+    )
+      //.subscribe( ({ id }) => console.log(id));
+
+      .subscribe ( heroe => this.heroe = heroe );
+  }
+
+
+
+    guardar(){
+      console.log(this.heroe);
+      if( this.heroe.superhero.trim().length === 0) {
+        return;
+      }
+
+      if( this.heroe.id ) {
+        this.heroesService.actualizarHeroe (this.heroe)
+          .subscribe( heroe => console.log( 'Actualizando', heroe))
+      }else{
+        //Crear
+        this.heroesService.agregarHeroe ( this.heroe )
+        .subscribe ( heroe => {
+          //console.log('Respuesta: ', resp);
+          this.router.navigate(['/heroes/editar', heroe.id]);
+        })
+      }
+/*
+this.heroesService.agregarHeroe ( this.heroe )
+        .subscribe ( resp => {
+          console.log('Respuesta: ', resp);
+        })
+*/
     }
 }
