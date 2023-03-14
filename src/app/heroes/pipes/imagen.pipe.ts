6301324d0a1406e7c55725bd9a045e8434be4f60
cbarriuso <carlosbarriuso@yahoo.es>
2023-03-14 12:01:18 +0100

    pipe puro y pipe impuro

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -2,12 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  // pure: true (es el valor por defecto, solo cambia si hay cambios en el
+  // argumento, en este caso no, porque es un objeto,  solo cambia una propiedad del
+  // objeto, no el objeto, que es el argumento)
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(heroe: Heroe ): string {
-    //console.log(heroe);
+    //console.log('Pipe imagen se proceso');
     if( !heroe.id && !heroe.alt_img ) {
       return 'assets/no-image.png';
     }else if( heroe.alt_img ) {
@@ -16,7 +19,7 @@ export class ImagenPipe implements PipeTransform {
       return `assets/heroes/${ heroe.id }.jpg`;
     }
 
-    return './assets/heroes/'+heroe.id+ '.jpg';
+    //return './assets/heroes/'+heroe.id+ '.jpg';
   }
 
 }
